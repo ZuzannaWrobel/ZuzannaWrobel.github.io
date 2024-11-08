61e76227b03cb41f697de3045d4ccbe15a37ade2
ZuzannaWrobel <zzwrobel@knox.edu>
2024-11-08 17:45:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+  header.addEventListener("click", () => {
+    header.style.backgroundColor = header.style.backgroundColor === "blue" ? "#333" : "blue";
+  });
+});
